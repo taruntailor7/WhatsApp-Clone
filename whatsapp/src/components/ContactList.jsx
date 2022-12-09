6d3ab632ba37e2bf325553bd677ad0d489c34cb3
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-09 19:40:58 +0530

    stated working on whatsapp clone

--- whatsapp/src/components/ContactList.jsx
+++ whatsapp/src/components/ContactList.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
 import styled from "styled-components"
+
 const Container = styled.div`
     display: flex;
     flex-direction:column;
     height: 100%;
+    width:30%
+`;
+
+const ProfileInfoDiv = styled.div`
+    display: flex;
+    flex-direction:column;
+    background: #ededed;
+    padding: 10px;
+`;
+
+const ProfileImage = styled.img`
+    width:32px;
+    height: 32px;
+    border-radius: 50%;
+`;
+
+const SearchBox = styled.div`
+    background: #f6f6f6;
+    padding: 10px;
+`;
+
+const SearchContainer = styled.div`
+    display: flex;
+    flex-direction:row;
+    background : white;
+    border-radius: 16px;
+    width: 100%;
+    padding: 5px 10px;
+`;
+
+const SearchIcon = styled.img`
+    width: 28px;
+    height: 28px;
 `;
 
 export const ContactList = () => {
   return (
-    <Container>ContactList</Container>
+    <Container>
+        <ProfileInfoDiv>
+            <ProfileImage src="/profile/elon.jpeg"/>
+        </ProfileInfoDiv>
+        <SearchBox>
+            <SearchContainer>
+                <SearchIcon src={"/search-icon.svg"} />
+
+            </SearchContainer>
+        </SearchBox>
+        ContactList
+    </Container>
   )
 }
--- whatsapp/src/components/Conversion.jsx
+++ whatsapp/src/components/Conversion.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+    flex-direction:column;
+    height: 100%;
+    width:70%;
+    background: #f6f7f8;
+`;
 
 export const Conversion = () => {
   return (
-    <div>Conversion</div>
+    <Container>Conversion</Container>
   )
 }
