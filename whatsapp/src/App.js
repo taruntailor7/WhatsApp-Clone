26246a8c652e767b3423bf7d8d21ab479a12cb6b
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-09 18:50:17 +0530

    stated working on whatsapp clone

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
+import { ContactList } from "./components/ContactList";
+import { Conversion } from "./components/Conversion";
+
+const Container = styled.div`
+  display : flex;
+  flex-direction: row;
+  height:100vh;
+  width:100%;
+  background: #f8f9fb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ContactList />
+      <Conversion />
+    </Container>
   );
 }
 
---
+++ whatsapp/src/components/ContactList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from "styled-components"
+const Container = styled.div`
+    display: flex;
+    flex-direction:column;
+    height: 100%;
+`;
+
+export const ContactList = () => {
+  return (
+    <Container>ContactList</Container>
+  )
+}
