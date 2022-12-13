8824b2280935da7bb7782449ea4b2ba105b4269f
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-13 23:15:43 +0530

    placeholder img adding

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -13,14 +13,19 @@ const Container = styled.div`
 
 const Placeholder = styled.div`
   display : flex;
-  flex-direction: coloum;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   font-size:14px;
   color:rgba(0,0,0,0.45);
+  gap:10px;
+  span{
+    font-size:32px;
+    color:#525252;
+  }
 `;
 
-const ChatPlaceholder = styled.div`
+const ChatPlaceholder = styled.img`
   width:240px;
   height:240px;
   border-radius:50%;
@@ -34,7 +39,9 @@ function App() {
     <Container>
       <ContactList />
       {selectedChat ? <Conversation /> : <Placeholder>
-          nnn
+        <ChatPlaceholder src="/welcome-placeholder.jpeg"/>
+        <span>Keep your phone connected</span>
+        WhatsApp connects to your phone to sync messages.
         </Placeholder>}
     </Container>
   );
