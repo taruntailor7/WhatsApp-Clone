bffb18e0f095a08f4567bf8ee3cbeff87b88fa2f
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-15 20:57:02 +0530

    starting work for today

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import { ContactList } from "./components/ContactList";
 import { Conversation } from "./components/Conversation";
 
