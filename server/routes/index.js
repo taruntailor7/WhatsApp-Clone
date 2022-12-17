55500b98f448df8162438135d90e26b09fcb438c
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-17 18:53:39 +0530

    routes done

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,32 @@ const applyRoutes = (app) => {
     app.get("/", (req, res) => {
         res.send({message:"hello"});
     })
+    // create-user, login, channel, search-user, channel-list, send-message
+
+    app.post("/user", (req, res) => {
+        res.send({message:"create-user"});
+    })
+
+    app.post("/login", (req, res) => {
+        res.send({message:"login"});
+    })
+
+    app.post("/channel", (req, res) => {
+        res.send({message:"channel"});
+    })
+    
+    app.get("/search-user", (req, res) => {
+        res.send({message:"search-user"});
+    })
+
+    app.get("/channel-list", (req, res) => {
+        res.send({message:"channel-list"});
+    })
+
+    app.post("/message", (req, res) => {
+        res.send({message:"message"});
+    })
+
 }
 
 export default applyRoutes;
