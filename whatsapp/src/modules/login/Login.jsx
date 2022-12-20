41d41dd103a378f35a29dd5480f3f5a9a798fdac
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-20 21:17:00 +0530

    login page

---
+++ whatsapp/src/modules/login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components";
+
+const container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    background-color: #0a0e11;
+    height: 100vh;
+`
+
+export const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
