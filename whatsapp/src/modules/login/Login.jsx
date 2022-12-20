66c5af9971bb8616a9ddef0198ebc3bf371ee858
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-20 22:38:39 +0530

    login page completed

--- whatsapp/src/modules/login/Login.jsx
+++ whatsapp/src/modules/login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from "styled-components";
+import { GoogleLogin } from 'react-google-login';
 
 const Container = styled.div`
     display: flex;
@@ -31,12 +32,59 @@ const CardView = styled.div`
     flex-wrap: wrap;
     color:black; 
 `
+const Head = styled.span`
+    padding:20px;
+    font-size:16px;
+`
+const Instruction = styled.div`
+    padding:20px;
+    font-size:16px;
+    color:#525252;
+    ol{
+        margin:40px 0px;
+    }
+    li{
+        margin:15px 0;
+    }
+`
+const Heading = styled.span`
+    font-size:24px;
+    color:#525252;
+`
+const QRCode = styled.img`
+    width:350px;
+    height:264px;
+    background-color:white;
+`
 
 export const Login = () => {
-  return (
-    <Container>
-        <Header>WhatsApp Web Clone</Header>
-        <CardView>Hello</CardView>
-    </Container>
-  )
+    const handleResponseFromGoogle = (responseData)=>{
+        console.log(responseData);
+    }
+
+    return (
+        <Container>
+            <Header>
+                <Head>WhatsApp Web Clone</Head>
+            </Header>
+            <CardView>
+                <Instruction>
+                    <Heading>To use WhatsApp on your computer:</Heading>
+                    <ol>
+                        <li>You need to Signin using your Google Account.</li>
+                        <li>You can anytime logout form the web.</li>
+                        <li>Click on the sign in button to continue using the WhatsApp Clone.</li>
+                    </ol>
+                    <GoogleLogin
+                        clientId="569591364082-2nlppfb3tvja16sol1gbp573il06kmue.apps.googleusercontent.com"
+                        buttonText="Sign In with Google"
+                        onSuccess={handleResponseFromGoogle}
+                        onFailure={handleResponseFromGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                </Instruction>
+                <QRCode src="https://www.tech-recipes.com/wp-content/uploads/2020/07/feature-image.jpg"/>
+            </CardView>
+        </Container>
+    )
 }
