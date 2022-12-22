f3eb2f805a496c8f384c178e63c272a666bd01eb
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-22 16:41:37 +0530

    now i will connect backend with frontend

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import configureExpressApp from "./config/index.js";
 import connection  from "./dbConnection/db.js";
 import applyRoutes from "./routes/index.js";
+import cors from "cors"
 
 const app = new express();
-configureExpressApp(app)
+configureExpressApp(app);
 
+app.use(express.json()); 
+app.use(cors());
 
 
 app.listen(3050, ()=>{
